10a533e96a640e32763e743708d08c8e42ad0c9a
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-21 12:59:51 +0300

    Enable user to display their profile bio after successfull authentication

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import Pitches,Comments,User
-from .forms import PitchForm,CommentForm
+from .forms import PitchForm,CommentForm,UpdateProfile
+from .. import db
 from flask_login import login_required
 
 @main.route("/")
@@ -45,4 +46,33 @@ def new_comment():
 
     title = "comments"
     return render_template('comments.html',title = title, comment_form=form,commentss=commentss)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
 
