6dd96096da2a9effc4aad91245f23a6a5754460f
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-21 13:24:47 +0300

    Created a welcome functionality that sends a welcome message once the user successfully register his credentials

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from . import auth
 from ..models import User
 from .. import db
 from .forms import LoginForm,RegistrationForm
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -26,7 +27,11 @@ def register():
         user = User(email = form.email.data,username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to Pitch App","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
+
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
 
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 export SECRET_KEY="statistics@109"
+export MAIL_USERNAME="johngicho@gmail.com"
+export MAIL_PASSWORD="Mathematics109"
 python3 manage.py server
